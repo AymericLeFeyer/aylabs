97cb4d188509a68d87760341f28d14f562168088
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2025-08-20 17:15:35 +0200

    fix(media-kit): update toFixed

--- src/components/MediaKitSection.tsx
+++ src/components/MediaKitSection.tsx
@@ -23,9 +23,9 @@ export const MediaKitSection: React.FC = () => {
 
   const formatNumber = (num: number) => {
     if (num >= 1000000) {
-      return `${(num / 1000000).toFixed(1)}M`;
+      return `${(num / 1000000).toFixed(2)}M`;
     } else if (num >= 1000) {
-      return `${(num / 1000).toFixed(1)}K`;
+      return `${(num / 1000).toFixed(2)}K`;
     }
     return num.toString();
   };
